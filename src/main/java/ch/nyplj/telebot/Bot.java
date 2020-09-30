ee5002adf8717e4de98c41d33732c07a6f6066b3
TH3Z0DIAC <65352278+TH3Z0DIAC@users.noreply.github.com>
2020-09-30 10:40:52 +0200

    Working HelloWorld Telegram-Bot

--- src/main/java/ch/nyplj/telebot/Bot.java
+++ src/main/java/ch/nyplj/telebot/Bot.java
@@ -1,4 +1,4 @@
-package ch.nyplj.telebot.bot;
+package ch.nyplj.telebot;
 
 
 import org.telegram.abilitybots.api.bot.AbilityBot;
@@ -9,21 +9,30 @@ import static org.telegram.abilitybots.api.objects.Privacy.PUBLIC;
 
 public class Bot extends AbilityBot {
 
+    /**
+     *
+     * @param botToken token given by BotFather
+     * @param botUsername username given to Bot by BotFather
+     */
     protected Bot(String botToken, String botUsername) {
         super(botToken, botUsername);
     }
 
+    // Not important yet, but has to be implemented
     @Override
     public int creatorId() {
         return 434;
     }
 
+    // Method that answers "HELLO WORLD" to command /hello
     public Ability saysHelloWorld() {
-        return Ability.builder()
-                .name("hello")
-                .privacy(PUBLIC)
-                .locality(USER)
+        return Ability.builder() // Starting to build an ability (of the bot)
+                .name("hello") // What comes after the command token / - "slash"
+                .privacy(PUBLIC) // Privacy of the Command
+                .locality(USER) // Who can use the command (in this case just users, no groups etc. )
                 .input(0) // How many arguments command needs
+                // Silently (without logging) sending message -> needs ChatID to know to whom to send the message
+                // ChatID is given by msgContext (can be renamed)
                 .action(msgContext -> silent.send("HELLO WORLD", msgContext.chatId()))
                 .build();
     }
--- src/main/java/ch/nyplj/telebot/Main.java
+++ src/main/java/ch/nyplj/telebot/Main.java
@@ -1,26 +1,32 @@
 package ch.nyplj.telebot;
 
-import ch.nyplj.telebot.bot.Bot;
-import com.pengrad.telegrambot.TelegramBot;
-import com.pengrad.telegrambot.impl.TelegramBotClient;
-import com.pengrad.telegrambot.model.Message;
-import com.pengrad.telegrambot.request.BaseRequest;
-import com.pengrad.telegrambot.request.SetWebhook;
-import com.pengrad.telegrambot.response.BaseResponse;
-
-import java.io.File;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
 public class Main {
 
+    public static final String BOT_TOKEN = "1364220505:AAGPZNlO5i_7yxOMTu8_qcbZxgGEh7DNt-Y";
+    public static final String BOT_NAME = "FncyRainbowBot";
+
     public static void main(String[] args) {
-        Bot bot = new Bot("1364220505:AAGPZNlO5i_7yxOMTu8_qcbZxgGEh7DNt-Y");
+        // Initializes API Context that can be used later on
+        ApiContextInitializer.init();
+        // New Connection to TelegramBot API
+        TelegramBotsApi api = new TelegramBotsApi();
 
-        SetWebhook request = new SetWebhook()
-                .url("url").certificate(new byte[]{});
+        try {
+            // Register new Bot
+            // IMPORTANT: Bot has to be in same package as Main (Protected Access Constructor)
+            //  -> or Change Constructor Access Modifier to Public
+            api.registerBot(new Bot(BOT_TOKEN, BOT_NAME));
+
+            // ignored -> no warning that catch is empty
+        } catch (TelegramApiRequestException ignored) {
+
+        }
 
 
-        BaseResponse response = bot.execute(request);
-        System.out.println(response.isOk());
     }
 
 }
