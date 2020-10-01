1384d3922b70ab64fc5e6d0d99172fcdb1e5de85
TH3Z0DIAC <65352278+TH3Z0DIAC@users.noreply.github.com>
2020-10-01 09:06:17 +0200

    removed Botinformation

--- src/main/java/ch/nyplj/telebot/Bot.java
+++ src/main/java/ch/nyplj/telebot/Bot.java
@@ -1,16 +1,21 @@
 package ch.nyplj.telebot;
 
-
 import org.telegram.abilitybots.api.bot.AbilityBot;
 import org.telegram.abilitybots.api.objects.Ability;
+import org.telegram.abilitybots.api.objects.MessageContext;
+
+import java.nio.charset.StandardCharsets;
 
 import static org.telegram.abilitybots.api.objects.Locality.USER;
 import static org.telegram.abilitybots.api.objects.Privacy.PUBLIC;
 
 public class Bot extends AbilityBot {
+    private final int CREATOR_ID = 1252376620;
+    private java.util.function.Consumer<MessageContext> Consumer;
+    private Object MessageContext;
 
     /**
-     *
+     * Basic Bot Constructor
      * @param botToken token given by BotFather
      * @param botUsername username given to Bot by BotFather
      */
@@ -21,19 +26,48 @@ public class Bot extends AbilityBot {
     // Not important yet, but has to be implemented
     @Override
     public int creatorId() {
-        return 434;
+        return CREATOR_ID;
     }
 
     // Method that answers "HELLO WORLD" to command /hello
     public Ability saysHelloWorld() {
         return Ability.builder() // Starting to build an ability (of the bot)
                 .name("hello") // What comes after the command token / - "slash"
+                .info("Says hello world") // Information given when used /commands
                 .privacy(PUBLIC) // Privacy of the Command
                 .locality(USER) // Who can use the command (in this case just users, no groups etc. )
                 .input(0) // How many arguments command needs
                 // Silently (without logging) sending message -> needs ChatID to know to whom to send the message
                 // ChatID is given by msgContext (can be renamed)
-                .action(msgContext -> silent.send("HELLO WORLD", msgContext.chatId()))
+                .action(msgContext -> silent.send("HELLO MY WORLD", msgContext.chatId()))
+                .build();
+    }
+
+    public Ability saysGoodBye() {
+        return Ability.builder() // Starting to build an ability (of the bot)
+                .name("bye") // What comes after the command token / - "slash"
+                .privacy(PUBLIC) // Privacy of the Command
+                .locality(USER) // Who can use the command (in this case just users, no groups etc. )
+                .input(0) // How many arguments command needs
+                // Silently (without logging) sending message -> needs ChatID to know to whom to send the message
+                // ChatID is given by msgContext (can be renamed)
+                .action(msgContext -> {
+                    // Converting message String into UTF_8 for correct layout in Telegram
+                    silent.send(new String("TÜDELÜÜ".getBytes(), StandardCharsets.UTF_8), msgContext.chatId());
+                    System.out.println(msgContext.chatId());
+                })
+                .build();
+    }
+
+    public Ability saySomething() {
+        return Ability.builder() // Starting to build an ability (of the bot)
+                .name("saysomething") // What comes after the command token / - "slash"
+                .privacy(PUBLIC) // Privacy of the Command
+                .locality(USER) // Who can use the command (in this case just users, no groups etc. )
+                .input(0) // How many arguments command needs
+                // Silently (without logging) sending message -> needs ChatID to know to whom to send the message
+                // ChatID is given by msgContext (can be renamed)
+                .action(msgContext -> silent.send("I'm giving up to you", msgContext.chatId()))
                 .build();
     }
 
--- src/main/java/ch/nyplj/telebot/Main.java
+++ src/main/java/ch/nyplj/telebot/Main.java
@@ -4,14 +4,19 @@ import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
+import static ch.nyplj.telebot.BotInformation.BOT_NAME;
+import static ch.nyplj.telebot.BotInformation.BOT_TOKEN;
+
 public class Main {
 
-    public static final String BOT_TOKEN = "1364220505:AAGPZNlO5i_7yxOMTu8_qcbZxgGEh7DNt-Y";
-    public static final String BOT_NAME = "FncyRainbowBot";
+//    public static final String BOT_TOKEN = "1310849219:AAHDGvg_dhS7zE9tNqYp4seFw48Lu63uMxg";
+//    public static final String BOT_NAME = "ronjaK_bot";
 
     public static void main(String[] args) {
         // Initializes API Context that can be used later on
         ApiContextInitializer.init();
+
+
         // New Connection to TelegramBot API
         TelegramBotsApi api = new TelegramBotsApi();
 
@@ -19,13 +24,14 @@ public class Main {
             // Register new Bot
             // IMPORTANT: Bot has to be in same package as Main (Protected Access Constructor)
             //  -> or Change Constructor Access Modifier to Public
-            api.registerBot(new Bot(BOT_TOKEN, BOT_NAME));
+            Bot bot = new Bot(BOT_TOKEN, BOT_NAME);
+            api.registerBot(bot);
+
 
             // ignored -> no warning that catch is empty
         } catch (TelegramApiRequestException ignored) {
 
         }
-
 
     }
 
